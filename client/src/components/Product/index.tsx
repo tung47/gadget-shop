ad466d355093ffacc234e227ea04ce4258ca8c54
tung <tung.nguyen271193@gmail.com>
2020-10-09 12:31:53 +0300

    Implement react router

--- client/src/components/Product/index.tsx
+++ client/src/components/Product/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 
 import { ProductProps } from '../../types'
@@ -7,16 +8,16 @@ import Rating from '../Rating'
 const Product = ({ product }: ProductProps) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/products/${product._id}`}>
+      <Link to={`/products/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/products/${product._id}`}>
+        <Link to={`/products/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
           <Rating
