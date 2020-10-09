22bc7fbe06eefe3595b2126df3d6213b7cf0ba4a
tung <tung.nguyen271193@gmail.com>
2020-10-09 08:52:48 +0300

    add product list in HomeScreen

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -9,6 +9,22 @@ const ProductCard = ({ product }: Product) => {
       <a href={`/products/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
+
+      <Card.Body>
+        <a href={`/products/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as="div">
+          <div className="my-3">
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+
+        <Card.Text as="h3">€{product.price}</Card.Text>
+      </Card.Body>
     </Card>
   )
 }
