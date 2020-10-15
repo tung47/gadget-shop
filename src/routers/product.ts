f7392ac61d9b42cc0d965992bb15ba2a9e9756dc
tung <tung.nguyen271193@gmail.com>
2020-10-15 12:39:36 +0300

    add product controller

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -1,37 +1,11 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 
-import Product from '../models/Product'
+import { getProductById, getProducts } from '../controllers/product'
 
 const router = express.Router()
 
 // Every path we define here will get /api/v1/products prefix
-
-// @desc Fetch all products
-// @route GET /api/v1/products
-// @access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
-
-// @desc Fetch single product
-// @route GET /api/v1/product/:id
-// @access Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
